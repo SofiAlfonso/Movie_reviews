0ed40e2ba7a6d456906ce908f9c52e3af648e459
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-07-31 13:20:44 -0500

    Función busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
    # return HttpResponse('<center><h1>Welcome to Home Page :)</h1><center>')
    #return render (request,'home.html')
-   return render(request,'home.html',{'name':'Ana Sofia A.M'})
+   #return render(request,'home.html',{'name':'Ana Sofia A.M'})
+   searchTerm= request.GET.get('searchMovie')
+   if searchTerm:
+    movies= Movie.objects.filter(title__icontains=searchTerm)
+   else:
+    movies= Movie.objects.all()
+   return render(request,'home.html', {'searchterm':searchTerm, 'movies':movies})
+
 
 def About(request):
     #return HttpResponse('<center><h1>Welcome to About Page :)</h1><center>')
