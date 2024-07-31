4d3387fd93b49cda8facd4477b0c92dd68c1e084
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-07-31 11:10:32 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
    # return HttpResponse('<center><h1>Welcome to Home Page :)</h1><center>')
    #return render (request,'home.html')
-   return render(request,'home.html',{'name':'Ana Sofia'})
+   return render(request,'home.html',{'name':'Ana Sofia A.M'})
 
 def About(request):
     return HttpResponse('<center><h1>Welcome to About Page :)</h1><center>')
